9c6bb3cd711f34e69137013d26020453eb3947ba
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-01-26 03:25:49 +0200

    main

---
+++ filterObj.js
@@ -0,0 +1,44 @@
+const data = [
+  [
+    { "What is your marital status?": "Single" },
+    { "Are you planning on getting married next year?": "Yes/No" },
+  ],
+  [
+    { "What is your marital status?": "Married" },
+    { "How long have you been married?": "Less than a year" },
+  ],
+  [
+    { "What is your marital status?": "Married" },
+    { "How long have you been married?": "More than a year" },
+    { "Have you celebrated your one year anniversary?": "Yes/No" },
+  ],
+  [
+    { "What is your marital status?": "Single" },
+    { "Are you planning on getting married next year?": "Yes/No" },
+  ],
+  [
+    { "What is your marital status?": "Single" },
+    { "Are you planning on getting married next year?": "Yes/No" },
+  ],
+];
+// indexOf(JSON.stringify(item)) === indx;
+
+// const resultObn = data.filter((item, indx) => {
+//   return console.log(data.map(JSON.stringify).indexOf(JSON.stringify(item)));
+// });
+
+// const result = [
+//   [
+//     { "What is your marital status?": "Single" },
+//     { "Are you planning on getting married next year?": "Yes/No" },
+//   ],
+//   [
+//     { "What is your marital status?": "Married" },
+//     { "How long have you been married?": "Less than a year" },
+//   ],
+//   [
+//     { "What is your marital status?": "Married" },
+//     { "How long have you been married?": "More than a year" },
+//     { "Have you celebrated your one year anniversary?": "Yes/No" },
+//   ],
+// ];
--- reportSalary.js
+++ reportSalary.js
@@ -1,59 +1,89 @@
-const salaries1 = {
-  Manager: { salary: 1000, tax: "10%" },
-  Designer: { salary: 600, tax: "30%" },
-  Artist: { salary: 1500, tax: "15%" },
-};
-const team1 = [
-  { name: "Misha", specialization: "Manager" },
-  { name: "Max", specialization: "Designer" },
-  { name: "Vova", specialization: "Designer" },
-  { name: "Leo", specialization: "Artist" },
-];
+// const salaries1 = {
+//   Manager: { salary: 1000, tax: "10%" },
+//   Designer: { salary: 600, tax: "30%" },
+//   Artist: { salary: 1500, tax: "15%" },
+// };
+// const team1 = [
+//   { name: "Misha", specialization: "Manager" },
+//   { name: "Max", specialization: "Designer" },
+//   { name: "Vova", specialization: "Designer" },
+//   { name: "Leo", specialization: "Artist" },
+//   { name: "Misha", specialization: "Manager" },
+//   { name: "Max", specialization: "Designer" },
+//   { name: "Vova", specialization: "Designer" },
+//   { name: "Leo", specialization: "Artist" },
+//   { name: "Misha", specialization: "Manager" },
+//   { name: "Max", specialization: "Designer" },
+//   { name: "Vova", specialization: "Designer" },
+//   { name: "Leo", specialization: "Artist" },
+// ];
 
-const salaries2 = {
-  TeamLead: { salary: 1000, tax: "99%" },
-  Architect: { salary: 9000, tax: "34%" },
-};
-const team2 = [
-  { name: "Alexander", specialization: "TeamLead" },
-  { name: "Gaudi", specialization: "Architect" },
-  { name: "Koolhas", specialization: "Architect" },
-  { name: "Foster", specialization: "Architect" },
-  { name: "Napoleon", specialization: "General" },
-];
+// const salaries2 = {
+//   TeamLead: { salary: 1000, tax: "99%" },
+//   Architect: { salary: 9000, tax: "34%" },
+// };
+// const team2 = [
+//   { name: "Alexander", specialization: "TeamLead" },
+//   { name: "Gaudi", specialization: "Architect" },
+//   { name: "Koolhas", specialization: "Architect" },
+//   { name: "Foster", specialization: "Architect" },
+//   { name: "Napoleon", specialization: "General" },
+// ];
 
-function calculateTeam(team) {
-  const resultObj = {};
-  team.map(({ specialization }) => {
-    if (!resultObj[specialization]) {
-      resultObj[specialization] = 1;
-    } else {
-      resultObj[specialization] += 1;
-    }
-  });
-  return resultObj;
-}
+// function calculateTeam(team) {
+//   const resultObj = {};
+//   team.map(({ specialization }) => {
+//     if (!resultObj[specialization]) {
+//       resultObj[specialization] = 1;
+//     } else {
+//       resultObj[specialization] += 1;
+//     }
+//   });
+//   return resultObj;
+// }
 
-function calculateTeamFinanceReport(salaries, team) {
-  const teams = calculateTeam(team);
+// function calculateTeamFinanceReport(salaries, team) {
+//   const teams = calculateTeam(team);
 
-  const totalObj = {};
+//   const totalObj = {};
 
-  for (const item in salaries) {
-    if (Object.hasOwn(teams, item)) {
-      const { tax, salary } = salaries[item];
-      //   pecentage to integer
-      const percentage = 1 + parseFloat(tax) / 100;
-      //
-      const sum = Math.trunc(salary * teams[item] * percentage);
+//   for (const item in salaries) {
+//     if (Object.hasOwn(teams, item)) {
+//       const { tax, salary } = salaries[item];
 
-      totalObj["totalBadgetTeam"] = (totalObj["totalBadgetTeam"] || 0) + sum;
-      totalObj[`total${item}Team`] = sum;
-    }
+//       const percentage = 1 + parseFloat(tax) / 100;
+
+//       const sum = Math.trunc(salary * teams[item] * percentage);
+
+//       totalObj["totalBadgetTeam"] = (totalObj["totalBadgetTeam"] || 0) + sum;
+//       totalObj[`total${item}Team`] = sum;
+//     }
+//   }
+
+//   return totalObj;
+// }
+
+// console.log(calculateTeamFinanceReport(salaries1, team1));
+// console.log(calculateTeamFinanceReport(salaries2, team2));
+
+const arr = Array.from({ length: Math.random() * 1000 }, (_, idx) => idx);
+
+const recursiveFn = (array, nim) => {
+  let low = 0;
+  let hight = array.length - 1;
+  let middle = (hight + low) / 2;
+  console.log(middle);
+  if (array[middle] === nim) return middle;
+
+  if (array[middle] > nim) {
+    array.splice(middle - 1);
+    // recursiveFn(array, nim);
+  } else {
+    array.splice(middle + 1);
+    // recursiveFn(array, nim);
   }
+  return middle;
+};
 
-  return totalObj;
-}
-
-console.log(calculateTeamFinanceReport(salaries1, team1));
-console.log(calculateTeamFinanceReport(salaries2, team2));
+console.log(recursiveFn(arr, 55));
+console.log(arr);
