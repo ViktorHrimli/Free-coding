5dd5daf325f9d32d4500563138c85f95e9d9cb2d
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2024-04-11 23:54:59 +0300

    forget rome to int

--- benchMark.js
+++ benchMark.js
@@ -83,56 +83,6 @@
 // console.log(comonFuncFindIndex(["flower", "flow", "flight"]));
 // console.log(comonFuncFindIndex(["dog", "racecar", "car"]));
 
-const objMap = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-
-// var romanToInt = function (s) {
-//   var strArr = s.split("");
-//   var skipNext = false;
-
-//   return strArr.reduce((acc, el, ind, arr) => {
-//     if (skipNext) {
-//       skipNext = false;
-//       return acc;
-//     }
-//     var nextNumeral = objMap[arr[ind + 1]];
-//     var currentNumeral = objMap[el];
-
-//     if (nextNumeral > currentNumeral) {
-//       acc += nextNumeral - currentNumeral;
-//       skipNext = true;
-//     } else {
-//       acc += currentNumeral;
-//     }
-//     return acc;
-//   }, 0);
-// };
-
-// var romanToInt = (s) => {
-//   var res = 0;
-//   for (let i = 0; i < s.length; i++) {
-//     var currentNumeral = objMap[s[i]];
-//     var nextNumeral = objMap[s[i + 1]];
-
-//     if (currentNumeral < nextNumeral) {
-//       res += nextNumeral - currentNumeral;
-//       i++;
-//     } else {
-//       res += currentNumeral;
-//     }
-//   }
-//   return res;
-// };
-
-// console.log(romanToInt("MCMXCIV"));
-
 // '(', ')', '{', '}', '[' and ']
 
 // var objScoupe = {
